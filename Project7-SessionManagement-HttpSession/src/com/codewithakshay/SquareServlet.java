8f369e09f256061400e668531c265498fab1db21
Akshay <akhareakshay2@gmail.com>
2021-06-22 11:28:41 +0530

    Square Servlet

--- Project7-SessionManagement-HttpSession/src/com/codewithakshay/SquareServlet.java
+++ Project7-SessionManagement-HttpSession/src/com/codewithakshay/SquareServlet.java
@@ -7,15 +7,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-		int square = Integer.parseInt(req.getParameter("add"));
+		HttpSession session=req.getSession();
 
-		square = square * square;
+		int square = (int) session.getAttribute("add");
+
+		square=square*square;
 
 		PrintWriter writer = res.getWriter();
 		writer.println("Sqare is : " + square);

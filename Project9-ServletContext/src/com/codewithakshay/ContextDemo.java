dd58c2ae57951043322716daa18e819903c50995
Akshay <akhareakshay2@gmail.com>
2021-07-02 12:39:51 +0530

    source folder

---
+++ Project9-ServletContext/src/com/codewithakshay/ContextDemo.java
@@ -0,0 +1,35 @@
+package com.codewithakshay;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ContextDemo extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		// servletContext object
+		// we can create its object in two ways..
+		// 1. by using request object ref and
+		// ServletContext ctx = req.getServletContext();
+		// and directly also we can create
+		ServletContext ctx = getServletContext();
+		// both are correct
+		// can choose any one..
+
+		// here we are creating printWriter object to print the content on browser
+		PrintWriter out = res.getWriter();
+		out.print("Hii ");
+
+		String name = ctx.getInitParameter("name");
+		out.print(name);
+
+	}
+
+}
---
+++ Project9-ServletContext/src/com/codewithakshay/ContextDemo2.java
@@ -0,0 +1,30 @@
+package com.codewithakshay;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ContextDemo2 extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		ServletContext ctx = req.getServletContext();
+
+		PrintWriter out = res.getWriter();
+		out.print("Hii ");
+
+		String myName = ctx.getInitParameter("name");
+		out.print(myName);
+
+		String myAge = ctx.getInitParameter("age");
+		out.print(" your age is : " + myAge);
+
+	}
+
+}

4a7d5bc31e20768fab9cb61c3a39cc327feaf986
Akshay <akhareakshay2@gmail.com>
2021-07-11 12:53:55 +0530

    voter app servlet class

--- Project13-NTRJ-VoterApp/src/com/codewithakshay/VoterApp.java
+++ Project13-NTRJ-VoterApp/src/com/codewithakshay/VoterApp.java
@@ -10,15 +10,31 @@ import javax.servlet.http.HttpServletResponse;
 
 public class VoterApp extends HttpServlet {
 
+	/*
+	 * In this web application, we are going to see the communication between Html
+	 * to Servlet
+	 * 
+	 * so, there are three approaches to achieve communication
+	 * 
+	 * 1) using hyperlink 2) using submit button 3) using java script..
+	 * 
+	 * so, in this project, we've implemented first approach i.e. using hyperlink
+	 */
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
+		// set content type
 		res.setContentType("text/html");
+
+		// printwriter object
 		PrintWriter pw = res.getWriter();
 
+		// getting parameters from end user
 		String name = req.getParameter("pname");
 		int age = Integer.parseInt(req.getParameter("page"));
 
+		// checking the eligibility
 		if (age < 18) {
 			pw.println("<b><h4 style='color : red' >" + name + ", You're not Eligible for Voting <h4><b>");
 		} else {

caaba606f91965917ad0f3220ce047ef6bdb1914
Akshay <akhareakshay2@gmail.com>
2021-07-11 14:26:31 +0530

    marriage servlet java file

--- Project14-NTRJ-MarriageApp/src/com/codewithakshay/MarriageServlet.java
+++ Project14-NTRJ-MarriageApp/src/com/codewithakshay/MarriageServlet.java
@@ -1,3 +1,5 @@
+package com.codewithakshay;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -7,28 +9,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MarriageServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
-		//printWriter object
+
+		// printWriter object
 		PrintWriter pw = res.getWriter();
-		
-		//set content type
+
+		// set content type
 		res.setContentType("text/html");
-		
-		//getting inputs as a parameter from end user
+
+		// getting inputs as a parameter from end user
 		String name = req.getParameter("pname");
 		int age = Integer.parseInt(req.getParameter("page"));
 		String gender = req.getParameter("gender");
-		
-		if(age<18 && gender.equals("f")) {
-			pw.println("You are not Eligible for Marriage !");
+
+		/*
+		 * if(age<18 && gender.equals("f")) {
+		 * pw.println("You are not Eligible for Marriage !"); } else {
+		 * pw.println("You are Eligible for Marriage !"); } if(age<21 &&
+		 * gender.equals("m")) { pw.println("You are not Eligible for Marriage !"); }
+		 * else { pw.println("You are Eligible for Marriage !"); } if(age<21 &&
+		 * gender.equals("o")) { pw.println("You are not Eligible for Marriage !"); }
+		 * else { pw.println("You are Eligible for Marriage !"); }
+		 */
+
+		if (gender.equals("f")) {
+			if (age < 18) {
+				pw.println("You are not eligible");
+			} else {
+				pw.println("You are eligible");
+			}
+		} else if (gender.equals("m")) {
+			if (age < 21) {
+				pw.println("You are not eligible");
+			} else {
+				pw.println("You are eligible");
+			}
+		} else if (gender.equals("o")) {
+			if (age < 21) {
+				pw.println("You are not eligible");
+			} else {
+				pw.println("You are eligible");
+			}
 		}
-		else {
-			pw.println("You are Eligible for Marriage !");
-		}
-		
 	}
 
 }

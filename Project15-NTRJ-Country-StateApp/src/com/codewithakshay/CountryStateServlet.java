915afba166fc8d7e45a3de9b60f0acde9eac0670
Akshay <akhareakshay2@gmail.com>
2021-07-13 12:45:38 +0530

    source package

---
+++ Project15-NTRJ-Country-StateApp/src/com/codewithakshay/CountryStateServlet.java
@@ -0,0 +1,54 @@
+package com.codewithakshay;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CountryStateServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		// PrintWriter object
+		PrintWriter pw = res.getWriter();
+
+		// set content type
+		res.setContentType("text/html");
+
+		String country = req.getParameter("country");
+
+		String indiaStates[] = { "Maharashtra", "Andhra Pradesh", "Tamil Nadu", "Kerala", "Bihar", "Odisa" };
+		String usStates[] = { "Alabama", "California", "Florida", "Hawaii", "Indiana" };
+		String ukStates[] = { "England", "Scotland", "Wales", "Northern Ireland" };
+		String shriLankaStates[] = { "North  Eastern", "Anuradhapura", "Southern", "Kandy", "Western" };
+		String austreliaStates[] = { "Northern Territory", "Queensland", "South Australia", "Western Australia",
+				"Victoria" };
+
+		// get states based on the country selection
+		if (country.equals("India")) {
+			pw.println("<h2 style= 'color: red; font-family: monospace; text-align : center;'> States of India : "
+					+ Arrays.toString(indiaStates) + " <h2>");
+		} else if (country.equals("United States")) {
+			pw.println(
+					"<h2 style= 'color: red; font-family: monospace; text-align : center;'>States in United States : "
+							+ Arrays.toString(usStates) + " <h2>");
+		} else if (country.equals("United Kingdom")) {
+			pw.println(
+					"<h2 style= 'color: red; font-family: monospace; text-align : center;'>States in United Kingdom : "
+							+ Arrays.toString(ukStates) + " <h2>");
+		} else if (country.equals("ShriLanka")) {
+			pw.println("<h2 style= 'color: red; font-family: monospace; text-align : center;'>States in ShriLanka : "
+					+ Arrays.toString(shriLankaStates) + " <h2>");
+		} else if (country.equals("Austrelia")) {
+			pw.println("<h2 style= 'color: red; font-family: monospace; text-align : center;'>States in Austrelia : "
+					+ Arrays.toString(austreliaStates) + " <h2>");
+		}
+		pw.println("<a style = 'align : center; font-family: monospace;' href = 'Country-State.html'>Home</a>");
+	}
+
+}
